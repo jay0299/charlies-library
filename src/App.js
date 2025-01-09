78c7245ed23b59bcaff795f6b515db5722db0681
Jay <jeehyeon5317@gmail.com>
2025-01-09 10:16:55 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>이 리액트 앱은 이제 제 것입니다.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
